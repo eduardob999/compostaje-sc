e0211b9fab9b54066ff3563ceb84e4374779d5f9
eduardob999 <eduardob1999@gmail.com>
2023-10-04 11:51:03 -0400

    update

--- src/routes.jsx
+++ src/routes.jsx
@@ -14,31 +14,6 @@ export const routes = [
     path: "/home",
     element: <Home />,
   },
-  {
-    icon: UserCircleIcon,
-    name: "profile",
-    path: "/profile",
-    element: <Profile />,
-  },
-  {
-    icon: ArrowRightOnRectangleIcon,
-    name: "Sign In",
-    path: "/sign-in",
-    element: <SignIn />,
-  },
-  {
-    icon: UserPlusIcon,
-    name: "Sign Up",
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-  {
-    icon: DocumentTextIcon,
-    name: "Docs",
-    href: "https://www.material-tailwind.com/docs/react/installation",
-    target: "_blank",
-    element: "",
-  },
 ];
 
 export default routes;
--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -121,15 +121,12 @@ export function Navbar({ brandName, routes, action }) {
 }
 
 Navbar.defaultProps = {
-  brandName: "Material Tailwind React",
+  brandName: "Serviicio Comunitario - El copmpostaje - UCV",
   action: (
     <a
       href="https://www.creative-tim.com/product/material-tailwind-kit-react"
       target="_blank"
     >
-      <Button variant="gradient" size="sm" fullWidth>
-        free download
-      </Button>
     </a>
   ),
 };
